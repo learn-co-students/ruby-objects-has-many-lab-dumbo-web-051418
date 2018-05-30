630853c9cbc02264e70ea1e82eeed5c57f865907
Eric Razor, Jr <eric.razorjr@gmail.com>
2018-05-30 16:53:32 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,14 +16,14 @@ class Artist
     @songs
   end
   def add_song(song)
-     song.artist = self
+     song.artist = self  #creating an artist and adding to the
      @songs << song
      @@song_count += 1
   end
 
   def add_song_by_name(song_name)
     new_song = Song.new(song_name)
-    new_song.artist = self
+    new_song.artist = self #assigning a song to the instance of the artist from the name
     @songs << new_song
     @@song_count += 1
   end
